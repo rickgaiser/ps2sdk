cdad18650f464c89e0be266f6537440dcbd8c7e1
Rick Gaiser <rgaiser@gmail.com>
2018-04-28 21:57:17 +0200

    Add Block Device Manager (BDM)

---
+++ iop/fs/bdm/src/main.c
@@ -0,0 +1,39 @@
+#include <loadcore.h>
+#include <stdio.h>
+#include <irx.h>
+#include <bdm.h>
+
+//#define DEBUG  //comment out this line when not debugging
+#include "module_debug.h"
+
+
+#define MAJOR_VER 1
+#define MINOR_VER 1
+
+
+IRX_ID("bdm", MAJOR_VER, MINOR_VER);
+
+
+extern struct irx_export_table _exp_bdm;
+extern void bdm_init();
+extern void part_init();
+
+
+int _start( int argc, char *argv[])
+{
+	printf("Block Device Manager (BDM) v%d.%d\n", MAJOR_VER, MINOR_VER);
+
+	if (RegisterLibraryEntries(&_exp_bdm) != 0) {
+		M_PRINTF("ERROR: Already registered!\n");
+		return MODULE_NO_RESIDENT_END;
+	}
+
+	// initialize the block device manager
+	bdm_init();
+
+	// initialize the partition driver
+	part_init();
+
+	// return resident
+	return MODULE_RESIDENT_END;
+}
